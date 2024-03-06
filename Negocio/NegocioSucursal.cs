ef7607ae4ea1701cb99e820e6dd32dd74499001e
Lucas4d4 <101574260+Lucas4d4@users.noreply.github.com>
2024-03-06 18:00:22 -0300

    Add files via upload

---
+++ Negocio/NegocioSucursal.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Entidades;
+using DAO;
+using System.Data;
+
+namespace Negocio
+{
+    public static class NegocioSucursal
+    {
+        public static DataTable MostrarTablaSucursales(string id = "", bool x = true)
+        {
+
+            if (id == "" && !x)
+            {
+                return DaoSucursal.getTablaSucursal();
+            }
+            else
+            {
+                if (!Int32.TryParse(id, out int idNumber)) return null;
+                return DaoSucursal.getTablaSucursalIndividual(idNumber);
+            }
+
+        }
+
+        public static bool AgregarTablaSucursales(Sucursal suc)
+        {
+            //Retornar operación exitosa (se afectaron filas)
+            return (DaoSucursal.AgregarSucursal(suc) ?? 0) > 0;
+        }
+
+        public static int? elimSucursal(string id)
+        {
+            if (!Int32.TryParse(id, out int idNumber)) return null;
+            return DaoSucursal.EliminarSucursal(idNumber);
+        }
+
+    }
+}
