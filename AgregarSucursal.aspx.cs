14567f3f00281c6397652636bff1b6d0ecf29c17
Lucas4d4 <101574260+Lucas4d4@users.noreply.github.com>
2024-03-07 19:55:52 -0300

    Add files via upload

---
+++ AgregarSucursal.aspx.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using Entidades;
+using Negocio;
+namespace TP5_GRUPO_2
+{
+    public partial class Ejercicio1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Llenar dropdownlist de provincias
+            if (!IsPostBack) FillDDlProvincias();
+            // Reset labels y textboxes
+            lblError.Visible = lblConfirmar.Visible = false;
+            lblError.Text = "";
+        }
+
+        protected void FillDDlProvincias()
+        {
+            try
+            {
+                // Seleccionar del database la tabla de provincias
+                var dt = NegocioProvincia.SeleccionarTablaProvincias();
+                if (dt == null) throw new Exception("DB");
+
+                ddlProvinciaS.DataValueField = "ID_PROVINCIA";
+                ddlProvinciaS.DataTextField = "DESCRIPCIONPROVINCIA";
+                ddlProvinciaS.DataSource = dt;
+                ddlProvinciaS.DataBind();
+
+            }
+            catch(Exception ex)
+            {
+                lblError.Text += "Error al obtener listado de provincias.";
+                lblError.Visible = true;
+            }
+        }
+
+        protected bool ValidForm()
+        {
+            return
+                rfvDescripcion.IsValid &&
+                rfvDireccion.IsValid &&
+                rfvNombre.IsValid;
+        }
+
+        protected void btnAceptar_Click(object sender, EventArgs e)
+        {
+            // Validar mediante controles
+            if(ValidForm())
+            {
+                Sucursal suc = new Sucursal();
+                suc.setNombreSucursal(txtNombre.Text);
+                suc.setDescripcionSucursal(txtDescripcion.Text);
+                suc.setProvinciaSucursal(ProvinciaSeleccionada());
+                suc.setDireccionSucursal(txtDireccion.Text);
+
+                // Validar operación exitosa
+                if (NegocioSucursal.AgregarTablaSucursales(suc))
+                {
+                    // Reiniciar campos
+                    txtNombre.Text = txtDescripcion.Text = txtDireccion.Text = "";
+                    ddlProvinciaS.SelectedIndex = 0;
+                    // Colocar cursor en campo "Nombre"
+                    txtNombre.Focus();
+                    lblConfirmar.Text = "La sucursal se ha agregado con éxito";
+                    lblConfirmar.Visible = true;
+                }
+                else
+                {
+                    lblError.Text += "Error al guardar la sucursal.";
+                    lblError.Visible = true;
+                }
+            }
+            else
+            {
+                lblError.Text += "Formulario incompleto.";
+                lblError.Visible = true;
+            }
+        }
+
+        protected Provincia ProvinciaSeleccionada()
+        {
+            return new Provincia
+            (
+                Convert.ToInt32(ddlProvinciaS.SelectedValue),
+                ddlProvinciaS.SelectedItem.Text
+            );
+        }
+    }
+    
+}
---
+++ EliminarSucursal.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using Entidades;
+using Negocio;
+
+namespace TP5_GRUPO_2
+{
+    public partial class EliminarSucursal : System.Web.UI.Page
+    {
+        public void validar(string id)
+        {
+            int ? affected = NegocioSucursal.elimSucursal(id);
+            if (affected == 0)
+            {
+                Label1.Text = "ID inexistente";
+            }
+            else
+            {
+                if(affected == 1)
+                {
+                    Label1.Text = "La sucursal ha sido eliminada correctamente";
+                }
+                else
+                {
+                    Label1.Text = "Error al eliminar!";
+                }
+            }
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Label1.Text = "";
+        }
+
+        protected void btnEliminarID_Click(System.Object sender, System.EventArgs e)
+        {
+            if(PassesValidation())
+                validar(txtEliminarID.Text);
+        }
+
+        protected bool PassesValidation()
+        {
+            return rfvEliminar.IsValid &&
+                revEliminar.IsValid;
+        }
+    }
+    
+}
